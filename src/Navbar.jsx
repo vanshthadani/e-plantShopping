3d071d794dda556b756af6fb9aa937e8cd31c494
vanshthadani <vanshthadani2104@gmail.com>
2026-02-07 03:56:10 +0530

    Create Navbar.jsx

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+      <h2>Paradise Nursery</h2>
+      <div>
+        <span>Cart (0)</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
