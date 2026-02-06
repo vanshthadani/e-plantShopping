ffb01ef80eb661372886b21f9f1f48f5ede420d8
vanshthadani <vanshthadani2104@gmail.com>
2026-02-07 03:50:15 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery is your trusted destination for beautiful and healthy
+        houseplants. We believe that plants bring peace, freshness, and positive
+        energy into everyday life.
+      </p>
+      <p>
+        Our mission is to make plant shopping simple and enjoyable by offering a
+        wide variety of indoor plants along with an easy-to-use shopping
+        experience.
+      </p>
+      <p>
+        Whether you are a beginner or an experienced plant lover, Paradise
+        Nursery has something special for everyone.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
