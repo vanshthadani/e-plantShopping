404c681859fc41e3363f045561014aeaaa657916
vanshthadani <vanshthadani2104@gmail.com>
2026-02-07 04:08:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import LandingPage from "./components/LandingPage";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <ProductList />
-    </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </Router>
   );
 }
 
