aa7b6a90dab5076678d69f8eb6bd11f85a23717a
vanshthadani <vanshthadani2104@gmail.com>
2026-02-07 04:07:47 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", borderBottom: "1px solid #ccc" }}>
+    <nav
+      style={{
+        padding: "10px",
+        borderBottom: "1px solid #ccc",
+        display: "flex",
+        justifyContent: "space-between"
+      }}
+    >
       <h2>Paradise Nursery</h2>
+
       <div>
-        <span>Cart (0)</span>
+        <Link to="/products" style={{ marginRight: "15px" }}>
+          Products
+        </Link>
+        <Link to="/cart">Cart</Link>
       </div>
     </nav>
   );
